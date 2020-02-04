39c7982030c7219133dab5de5bed20eaa25d68e4
Felipe Díaz <afelipe00@users.noreply.github.com>
2020-02-03 21:39:11 -0500

    update diccionario opcion uno finalizada

--- Proyecto_c/Proyecto_c/diccionario.c
+++ Proyecto_c/Proyecto_c/diccionario.c
@@ -6,8 +6,8 @@
 
 struct palabra
 {
-	unsigned char nombre[30];
-	unsigned char sinonimo[30];
+    unsigned char nombre[30];
+    unsigned char sinonimo[30];
 };
 
 void listar(struct palabra dic[]) {
@@ -18,116 +18,146 @@ void buscar(struct palabra dic[]) {
 
 }
 
-int repeat(char word1[], char word2[]) {
-	int repe = 0;
-	int tam = 0;
-	for (int i = 0; i < 30; i++)
-	{
-		if (word1 != '0') {
-			tam = tam + 1;
-		}
-	}
-	for (int i = 0; i < tam; i++)
-	{
-		if (word1[i] != word2[i]) {
-			repe = 1;
-		}
-	}
-	return repe;
+int tam_pal(unsigned char word[]) {
+    int num_let = 0;
+    for (int i = 0; i < 30; i++)
+    {
+        if (word[i] != '0') {
+            num_let = num_let + 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return num_let;
+}
+int repeat(unsigned char word1[], unsigned char word2[]) {//recibe dos palabras y valida si son iguales
+    int repe = 0;//variable que retorna 1 si son diferentes 0 si son iguales
+    int tam = 0;//variable que identifica el tamaño de la palabra
+    tam = tam_pal(word1);//llamado funcion para identificar tamaño de la palabra
+    for (int i = 0; i < tam; i++)//for que identifica si las dos palabras son diferentes
+    {
+        if (word1[i] != word2[i]) {//validacion si el caracter es diferente
+            repe = 1;//si es diferente repe = 1
+        }
+    }
+    return repe;
 }
 
-void llenar(char word[], int opc) {
-	for (int i = 0; i < 30; i++)
-	{
-		word[i] = '0';
-	}
+void llenar(struct palabra dic[], unsigned char word[], int opc) {
+    //1 si es un vector , 0 si es la estructura
+    if (opc == 1) {//validacion de la opcion a llenar
+        for (int i = 0; i < 30; i++)//ciclo que recorre el vector de la palabra 30 porque es el maximo numero de letras
+        {
+            word[i] = '0';//llenado de 0
+        }
+    }
+    else {
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 30; j++) {
+                dic[i].nombre[j] = '0';
+                dic[i].sinonimo[j] = '0';
+            }
+        }
+    }
 }
 
 int num_palabras(struct palabra dic[]) {
-	int tam = 0;
-	for (int i = 0; i < 100; i++)
-	{
-		if (dic[i].nombre[0] == '0') {
-			tam = i;
-			break;
-		}
-	}
-	return tam;
+    int tam = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (dic[i].nombre[0] != '0') {
+            tam = tam + 1;
+
+        }
+    }
+    return tam;
 }
 
-void agregar(struct palabra dic[]) {
-	char opc = '1';
-	char word[30];
-	char repetida;
-	int tam = 0, npal = 0;
-	llenar(word,1);
-	do
-	{
-		printf("ingrese la palabra: ");
-		scanf_s("%s", &word);
-		npal = num_palabras(dic);
-		if (npal < 100) {
-			for (int i = 0; i < 100; i++) {
-				repetida = repeat(word, dic[i].nombre);
-				if (repetida == '0') {
-					printf("\npalabra existente en el diccionario\n");
-					break;
-				}
-				else
-				{
-					for (int j = 0; j < 30; j++)
-					{
-						if (word != '0') {
-							tam = tam + 1;
-						}
-						else
-						{
-							break;
-						}
-					}
-					for (int j = 0; j < tam; j++)
-					{
-						dic[i].nombre[j] = word[j];
-					}
-					opc = '0';
-				}
-			}
-		}
-		else
-		{
-			printf("\ndiccionario lleno\n");
-		}
-	} while (opc != '0');
+void agregar(struct palabra dic[], unsigned char word[]) {
+    char opc = '1';//variable de salida de ciclo
+    int repetida;//variable que me indica si la palabra ya esta en el diccionario
+    int num_let = 0, npal = 0;//tam = tamaño de la palabra y npal = numero de palabras en el diccionario
+    llenar(dic, word, 1);//llamado a la funcion llenar (llena con 0)
+    do//ciclo de funciones
+    {
+        printf("ingrese la palabra: ");
+        scanf_s("%s", word);//lectura de la palabra
+        num_let = tam_pal(word);//identificacion del tamaño de la palabra
+        npal = num_palabras(dic);//busqueda de cuanta palabras hay en el diccionario
+        for (int i = 0; i < num_let; i++) {
+            printf("%c ", word[i]);
+        }
+        if (npal < 100) {//si el diccionario no esta lleno
+            for (int i = 0; i < 100; i++) {//recorre todo el diccionario en busqueda de una palabra repetida
+                repetida = repeat(word, dic[i].nombre);//funcion repeat valida si las dos palabras son iguales
+                if (repetida == 0) {
+                    printf("\npalabra existente en el diccionario\n");
+                    break;
+                }
+            }
+            if (repetida != 0) {
+                for (int i = 0; i < num_let; i++) {
+                    dic[npal].nombre[i] = word[i];
+                }
+                printf("\npalabra ingresada\n");
+                printf("ingrese el sinonimo: ");
+                scanf_s("%s", word);
+                for (int i = 0; i < num_let; i++) {
+                    dic[npal].sinonimo[i] = word[i];
+                }
+                for (int i = 0; i < num_let; i++) {
+                    printf("%c ", word[i]);
+                }
+                printf("\nsinonimo ingresado\n");
+                system("pause");
+                opc = '0';
+            }
+        }
+        else
+        {
+            printf("\ndiccionario lleno\n");
+            opc = '0';
+        }
+    } while (opc != '0');
 }
 
 void menu_d() {
-	struct palabra diccionario[10];
-	char opc = '0';
-	do
-	{
-		printf("1.Agregar palabra\n2.Buscar\n3.listar");
-		printf("\ningrese la opcion: ");
-		scanf_s("%c", &opc);
-		switch (opc)
-		{
-		case '1':
-			agregar(diccionario);
-			break;
-		case '2':
-			buscar(diccionario);
-			break;
-		case '3':
-			listar(diccionario);
-			break;
-		case '0':
-			printf("Adios");
-		default:
-			printf("dato erroneo");
-			break;
-		}
-	} while (opc != '0');
+    struct palabra diccionario[100];//declaro la estructura del diccionario
+    unsigned char word[30];//vector donde se almacena la palabra escrita
+    char opc = '0';
+    char tra = '0';//variable que me maneja la opcion del menu
+    llenar(diccionario, word, 0);//llena el diccionario con 0
+    printf("--------------DICCIONARIO MASTER 3000-----------------\npresione enter para continuar");
+    do//ciclo de opciones
+    {
+        tra = getchar();
+        system("cls");
+        printf("--------------DICCIONARIO MASTER 3000-----------------\n");
+        printf("\n1.Agregar palabra\n2.Buscar\n3.listar");//impresion del menu
+        printf("\ningrese la opcion: ");
+        scanf_s("%c", &opc);//lectura de la opcion
+        switch (opc)//selector de casos
+        {
+        case '1':
+            agregar(diccionario, word);//llamado funcion agregar
+            break;
+        case '2':
+            buscar(diccionario);//llamado funcion buscar
+            break;
+        case '3':
+            listar(diccionario);//llamado funcion listar
+            break;
+        case '0':
+            printf("Adios");//caso de salida
+        default:
+            printf("dato erroneo");//impresion de dato erroneo
+            break;
+        }
+    } while (opc != '0');//opcion de 0 para salir
 }
 
 int main() {
-	menu_d();
+    menu_d();
 }
