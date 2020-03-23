c8a6b2822f18f6e5886cfebabe40e4c6bb532bb2
Felipe Díaz <afelipe00@users.noreply.github.com>
2020-03-23 00:57:02 -0500

    op desplazamiento

--- Proyectos Qt/op_desplazamiento/main.c
+++ Proyectos Qt/op_desplazamiento/main.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    char let = 'n';
+    int a = 3;
+    char mask = 0x01;
+    printf("variable sin corrimiento: %c\n",mask);
+    mask = mask << let;
+    printf("variable con corrimiento: %c\n",mask);
     return 0;
 }
